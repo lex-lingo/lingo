5c925386d3664dd9c57e0b42db5d98770b8e27ba
Jens Wille <jens.wille@uni-koeln.de>
2011-12-29 12:53:19 +0100

    Added bin/lingoctl script to control dictionaries and configurations for local Lingo directory.

---
+++ lib/lingo/ctl.rb
@@ -0,0 +1,171 @@
+require 'optparse'
+require 'fileutils'
+require 'nuggets/enumerable/minmax'
+
+class Lingo
+
+  module Ctl
+
+    extend self
+
+    PROG, VERSION = $0, '0.0.1'
+    PROGNAME = File.basename(PROG)
+
+    COMMANDS = {}
+
+    { config: %w[configuration],
+      lang:   %w[language],
+      dict:   %w[dictionary dictionaries],
+      store:  %w[store] }.each { |what, (sing, plur)|
+      COMMANDS["list#{what}"] = [
+        "List available #{plur || "#{sing}s"}",  'Arguments: [name...]'
+      ] if what != :store
+      COMMANDS["find#{what}"] = [
+        "Find #{sing} in Lingo search path",     'Arguments: name'
+      ]
+      COMMANDS["copy#{what}"] = [
+        "Copy #{sing} to local Lingo directory", 'Arguments: name'
+      ] if what != :store
+
+      %w[list find copy].each { |method|
+        class_eval %Q{def do_#{method}#{what}; #{method}(:#{what}); end}
+      }
+    }
+
+    COMMANDS.update(
+      'path'    => 'Print search path for dictionaries and configurations',
+      'help'    => 'Print help for available commands',
+      'version' => 'Print Lingo version number'
+    )
+
+    USAGE = <<EOT
+Usage: #{PROG} <command> [arguments] [options]
+       #{PROG} [-h|--help] [--version]
+EOT
+
+    OPTIONS = {}
+
+    def do
+      parse_options
+      send("do_#{COMMANDS.has_key?(command = ARGV.shift) ? command : 'usage'}")
+    end
+
+    private
+
+    def list(what)
+      names = Regexp.union(*ARGV.empty? ? '' : ARGV)
+
+      Lingo.list(what, path: path_for_scope).each { |file|
+        puts file if File.basename(file) =~ names
+      }
+    end
+
+    def find(what, doit = true)
+      name = ARGV.shift or do_usage('Required argument `name\' missing.')
+      no_args
+
+      file = Lingo.find(what, name, path: path_for_scope, &method(:do_usage))
+      doit ? puts(file) : file
+    end
+
+    def copy(what)
+      do_usage('Source and target are the same.') if OPTIONS[:scope] == :local
+
+      source = find(what, false)
+      target = File.join(path_for_scope(:local), Lingo.basepath(what, source))
+
+      FileUtils.mkdir_p(File.dirname(target))
+      FileUtils.cp(source, target, verbose: true)
+    end
+
+    def do_path
+      no_args
+      puts path_for_scope || PATH
+    end
+
+    def do_help(opts = nil)
+      no_args
+
+      msg = %w[Commands:]
+      msg.unshift(opts) if opts
+
+      max = COMMANDS.keys.max(:length)
+
+      COMMANDS.each { |command, description|
+        description = [*description]
+        msg << "  %-#{max}s - %s" % [command, description.shift]
+
+        description.each { |extra|
+          msg <<  "  %#{max}s   + %s" % [' ', extra]
+        } unless opts
+      }
+
+      abort msg.join("\n")
+    end
+
+    def do_version(doit = true)
+      no_args
+
+      msg = "Lingo v#{Lingo::VERSION}"
+      doit ? puts(msg) : msg
+    end
+
+    def do_usage(msg = nil)
+      msg = msg ? "#{PROGNAME}: #{msg}\n\n" : ''
+      abort msg << USAGE
+    end
+
+    def parse_options
+      OptionParser.new(USAGE, 14) { |opts|
+        opts.separator ''
+        opts.separator 'Scope options:'
+
+        opts.on('--system', 'Restrict command to the system-wide Lingo directory') {
+          OPTIONS[:scope] = :system
+        }
+
+        opts.on('--global', 'Restrict command to the user\'s personal Lingo directory') {
+          OPTIONS[:scope] = :global
+        }
+
+        opts.on('--local', 'Restrict command to the local Lingo directory') {
+          OPTIONS[:scope] = :local
+        }
+
+        opts.separator ''
+        opts.separator 'Generic options:'
+
+        opts.on('-h', '--help', 'Print this help message and exit') {
+          do_help(opts)
+        }
+
+        opts.on('--version', 'Print program version and exit') {
+          abort "#{PROGNAME} v#{VERSION} (#{do_version(false)})"
+        }
+      }.parse!
+    end
+
+    def path_for_scope(scope = OPTIONS[:scope])
+      case scope
+        when :system then [BASE]
+        when :global then [HOME]
+        when :local  then [CURR]
+        when nil
+        else do_usage("Invalid scope `#{scope.inspect}'.")
+      end
+    end
+
+    def no_args
+      do_usage('Too many arguments.') unless ARGV.empty?
+    end
+
+  end
+
+  def self.ctl
+    Ctl.do
+  rescue => err
+    raise if $VERBOSE
+    abort "#{err.backtrace.first}: #{err} (#{err.class})"
+  end
+
+end
