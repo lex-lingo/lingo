lex-lingo/lingo

commit 84efcdca3d91c7b17ab50c76146ae9e50cfcf878

lib/lingo/attendee/text_reader.rb: Accept directories and globs.

lib/lingo/attendee/text_reader.rb
@@ -24,6 +24,8 @@
 ###############################################################################
 #++
 
+require 'find'
+
 %w[filemagic mime/types hpricot pdf-reader].each { |lib|
   begin
     require lib
@@ -109,7 +111,8 @@ class Lingo
       # TODO: lir-record-pattern abkürzen
       # Interpretation der Parameter
       def init
-        @files    = Array(get_key('files', '-'))
+        get_files
+
         @chomp    = get_key('chomp', true)
         @filter   = get_key('filter', false)
         @progress = get_key('progress', false)
@@ -130,8 +133,6 @@ class Lingo
       # Gibt eine Datei zeilenweise in den Ausgabekanal
       def spool(path)
         unless stdin = stdin?(path)
-          raise FileNotFoundError.new(path) unless File.exist?(path)
-
           inc('Anzahl Dateien')
           add('Anzahl Bytes', size = File.size(path))
 
@@ -210,6 +211,27 @@ class Lingo
 
       def stdin?(path)
         %w[STDIN -].include?(path)
+      end
+
+      def get_files
+        args = [get_key('glob', '*.txt'), get_key('recursive', false)]
+
+        @files = []
+
+        Array(get_key('files', '-')).each { |path|
+          stdin?(path) ? @files << path : add_files(path, *args)
+        }
+
+        @files.map!(&File.method(:expand_path))
+        @files.uniq!
+      end
+
+      def add_files(path, glob, recursive = false)
+        Dir[path].sort!.each { |match|
+          File.directory?(match) ? recursive ? Find.find(match) { |entry|
+            @files << entry if File.file?(entry) && File.fnmatch?(glob, entry)
+          } : add_files(File.join(match, glob), glob) : @files << match
+        }.empty? and raise FileNotFoundError.new(path)
       end
 
       class PDFFilter

test/attendee/ts_text_reader.rb
@@ -6,7 +6,7 @@ class TestAttendeeTextReader < AttendeeTestCase
 
   def test_lir_file
     meet({ 'files' => 'test/lir.txt', 'lir-record-pattern' => '^\[(\d+)\.\]' }, nil, [
-      ai('LIR-FORMAT|'), ai('FILE|test/lir.txt'),
+      ai('LIR-FORMAT|'), ai("FILE|#{path = File.expand_path('test/lir.txt')}"),
       ai('RECORD|00237'),
       '020: GERHARD.',
       '025: Automatisches Sammeln, Klassifizieren und Indexieren von wissenschaftlich relevanten Informationsressourcen.',
@@ -17,13 +17,13 @@ class TestAttendeeTextReader < AttendeeTestCase
       ai('RECORD|00239'),
       '020: Information Retrieval und Dokumentmanagement im Multimedia-Zeitalter.',
       '056: "Das Buch ist ein praxisbezogenes VADEMECUM für alle, die in einer Welt der Datennetze Wissen/Informationen sammeln.',
-      ai('EOF|test/lir.txt')
+      ai("EOF|#{path}")
     ])
   end
 
   def test_lir_file_another_pattern
     meet({ 'files' => 'test/lir2.txt', 'lir-record-pattern' => '^\021(\d+)\022' }, nil, [
-      ai('LIR-FORMAT|'), ai('FILE|test/lir2.txt'),
+      ai('LIR-FORMAT|'), ai("FILE|#{path = File.expand_path('test/lir2.txt')}"),
       ai('RECORD|00237'),
       '020: GERHARD.',
       '025: Automatisches Sammeln, Klassifizieren und Indexieren von wissenschaftlich relevanten Informationsressourcen.',
@@ -34,15 +34,15 @@ class TestAttendeeTextReader < AttendeeTestCase
       ai('RECORD|00239'),
       '020: Information Retrieval und Dokumentmanagement im Multimedia-Zeitalter.',
       '056: "Das Buch ist ein praxisbezogenes VADEMECUM für alle, die in einer Welt der Datennetze Wissen/Informationen sammeln.',
-      ai('EOF|test/lir2.txt')
+      ai("EOF|#{path}")
     ])
   end
 
   def test_normal_file
     meet({ 'files' => 'test/mul.txt' }, nil, [
-      ai('FILE|test/mul.txt'),
+      ai("FILE|#{path = File.expand_path('test/mul.txt')}"),
       'Die abstrakte Kunst ist schön.',
-      ai('EOF|test/mul.txt')
+      ai("EOF|#{path}")
     ])
   end
 

commit fd6c2c9dc80b58af863994bed5518dcba96be1b7

lib/lingo/attendee/text_reader.rb: [FIX] Don't expand STDIN "path".
Regression introduced in 84efcdc. Also no longer uniq() file list.

lib/lingo/attendee/text_reader.rb
@@ -220,17 +220,27 @@ class Lingo
         Array(get_key('files', '-')).each { |path|
           stdin?(path) ? @files << path : add_files(path, *args)
         }
-
-        @files.map!(&File.method(:expand_path))
-        @files.uniq!
       end
 
       def add_files(path, glob, recursive = false)
-        Dir[path].sort!.each { |match|
-          File.directory?(match) ? recursive ? Find.find(match) { |entry|
-            @files << entry if File.file?(entry) && File.fnmatch?(glob, entry)
-          } : add_files(File.join(match, glob), glob) : @files << match
-        }.empty? and raise FileNotFoundError.new(path)
+        entries = Dir[path].sort!
+        raise FileNotFoundError.new(path) if entries.empty?
+
+        entries.each { |entry|
+          if File.directory?(entry)
+            if recursive
+              Find.find(entry) { |match|
+                if File.file?(match) && File.fnmatch?(glob, match)
+                  @files << File.expand_path(match)
+                end
+              }
+            else
+              add_files(File.join(entry, glob), glob)
+            end
+          else
+            @files << File.expand_path(entry)
+          end
+        }
       end
 
       class PDFFilter