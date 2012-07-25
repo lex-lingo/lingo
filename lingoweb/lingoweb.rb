a9498378e1d8dbb65b3ef46415fa8e7c0bd4a591
Jens Wille <jens.wille@uni-koeln.de>
2012-07-25 16:35:09 +0200

    lingoweb/lingoweb.rb: Lingo.call instead of spawn.

--- lingoweb/lingoweb.rb
+++ lingoweb/lingoweb.rb
@@ -1,63 +1,33 @@
 #! /usr/bin/env ruby
-# encoding: utf-8
 
-# Usage:
-#   ruby lingoweb.rb [-p <port>]
-#
-# or:
-#   rackup [-p <port>] lingoweb.rb
-
+require 'lingo'
 require 'sinatra'
 require 'nuggets/util/ruby'
 
-LINGOWEB = File.expand_path('..', __FILE__)
-LINGO    = File.join(File.dirname(LINGOWEB), 'lingo.rb')
-AUTH     = File.join(LINGOWEB, 'lingoweb.auth')
-CFG      = File.join(LINGOWEB, 'lingoweb.cfg')
-
-require LINGO
-
-LANGS = Dir["#{File.dirname(LINGO)}/*.lang"].map { |path|
-  path[%r{.*/(\w+)\.}, 1]
-}.sort
-
-abort 'No *.lang!' if LANGS.empty?
-
 UILANGS = %w[en de]
 
-if File.readable?(AUTH)
-  CREDS = File.read(AUTH).chomp.split(':', 2)
-else
-  if STDIN.tty?
-    require 'highline/import'
+LANGS = Lingo.list(:lang).map { |l| l[%r{.*/(\w+)\.}, 1] }.sort
+abort 'No *.lang!' if LANGS.empty?
 
-    user = ask('Enter user name [Leave empty to allow anyone]: ')
-    pass = ask('Enter password: ') { |q| q.echo = false } unless user.empty?
-  end
+base = __FILE__.sub(/\.[^.]*\z/, '')
+auth, cfg = base + '.auth', base + '.cfg'
 
-  CREDS = pass ? [user, pass] : []
+LINGO = Hash.new { |h, k| h[k] = Lingo.call(cfg, ['-l', k]) }
 
-  File.open(AUTH, 'w') { |f| f.write(CREDS.join(':')) }
+if File.readable?(auth)
+  c = File.read(auth).chomp.split(':', 2)
+  use(Rack::Auth::Basic) { |*b| b == c } unless c.empty?
 end
 
-use Rack::Auth::Basic do |*creds|
-  creds == CREDS
-end unless CREDS.empty?
-
 before do
-  hals = if hal = env['HTTP_ACCEPT_LANGUAGE']
-    hal.split(',').map { |l| l.split('-').first.strip } & UILANGS
-  else
-    []
-  end
-
-  @uilang = hals.first || UILANGS.first
+  @uilang = if hal = env['HTTP_ACCEPT_LANGUAGE']
+    hals = hal.split(',').map { |l| l.split('-').first.strip }
+    (hals & UILANGS).first
+  end || UILANGS.first
 
   @in   = params[:in]   || ''
   @lang = params[:lang] || @uilang
   @lang = LANGS.first unless LANGS.include?(@lang)
-
-  @success = true
 end
 
 get('')   { redirect url_for('/') }
@@ -69,19 +39,13 @@ helpers do
     "#{request.script_name}#{path}"
   end
 
-  def t(*trans)
-    i = UILANGS.index(@uilang)
-    i && trans[i] || trans.first
+  def t(*t)
+    (i = UILANGS.index(@uilang)) && t[i] || t.first
   end
 end
 
 def doit
-  @success = Process.ruby(LINGO, '-c', CFG, '-l', @lang) { |_, i, o, e|
-    IO.interact({ @in => i }, { o => @out = '', e => @err = '' })
-  }.success? unless @in.empty?
-
-  [@in, @out, @err].each { |x| x.force_encoding(Lingo::ENC) if x }
-
+  @out = LINGO[@lang].talk(@in) { |_| _ }.join("\n") unless @in.empty?
   erb :index
 end
 
