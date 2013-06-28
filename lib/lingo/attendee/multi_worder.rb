lex-lingo/lingo

commit 64877cfb108775d43c22d2e1a5468bb87db80bc5

lib/lingo/attendee/multi_worder.rb (check_multiword_key): Minimize work done in loop.

lib/lingo/attendee/multi_worder.rb
@@ -177,7 +177,9 @@ class Lingo
       def check_multiword_key(len)
         return [] if valid_tokens_in_buffer < len
 
-        seq = @buffer.map { |obj|
+        seq = []
+
+        @buffer.each { |obj|
           next [obj] unless obj.is_a?(WordForm)
           next if (form = obj.form) == CHAR_PUNCT
 
@@ -189,17 +191,20 @@ class Lingo
           @syn_dic.find_synonyms(w, i) if @syn_dic
           i.map! { |j| Unicode.downcase(j.form) }.uniq!
 
-          i
+          seq << i
+
+          break unless seq.length < len
         }
 
-        seq.compact!
-        seq.slice!(len..-1)
-
         if @combine
-          [].tap { |mul| seq.shift.product(*seq) { |key|
-            mul.concat(@mul_dic.select(key.join(' ')))
+          mul = []
+
+          seq.shift.product(*seq) { |key|
+            @mul_dic.select(key.join(' '), mul)
             break unless @all_keys || mul.empty?
-          } && mul.uniq! }
+          } && mul.uniq!
+
+          mul
         else
           @mul_dic.select(seq.map! { |i,| i }.join(' '))
         end

lib/lingo/attendee/variator.rb
@@ -97,8 +97,14 @@ class Lingo
         if obj.is_a?(Word) && @check[obj.attr]
           inc('Anzahl gesuchter Wörter')
 
-          @var.each_with_object([obj.form]) { |a, v| variate(v, *a) }.
-            tap { |v| v.slice!(@max..-1) }.each { |var|
+          vars, max = [obj.form], @max
+
+          @var.each { |args|
+            variate(vars, *args)
+            break unless vars.length < max
+          }
+
+          vars.each { |var|
             next if (word = find_word(var)).unknown? || (
               word.attr == WA_COMPOUND && word.lexicals.any? { |lex|
                 lex.attr.start_with?(LA_TAKEITASIS)

commit 325d588e5801abdd7093bffe442ef0bd751a2a81

lib/lingo/attendee/multi_worder.rb (check_multiword_key): Correcting small glitch introduced in 64877cf.

lib/lingo/attendee/multi_worder.rb
@@ -180,7 +180,7 @@ class Lingo
         seq = []
 
         @buffer.each { |obj|
-          next [obj] unless obj.is_a?(WordForm)
+          next seq << [obj] unless obj.is_a?(WordForm)
           next if (form = obj.form) == CHAR_PUNCT
 
           w = find_word(form, @lex_dic, @lex_gra)