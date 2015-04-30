f13de7160ecdfca4b5978c5cb31dc5c2c3076a67
Jens Wille <jens.wille@gmail.com>
2015-04-30 11:05:08 +0200

    lib/lingo/attendee/debug_filter.rb: Extracted from lib/lingo/attendee/debugger.rb.

--- lib/lingo/attendee/debugger.rb
+++ lib/lingo/attendee/debugger.rb
@@ -6,7 +6,7 @@
 # Lingo -- A full-featured automatic indexing system                          #
 #                                                                             #
 # Copyright (C) 2005-2007 John Vorhauer                                       #
-# Copyright (C) 2007-2014 John Vorhauer, Jens Wille                           #
+# Copyright (C) 2007-2015 John Vorhauer, Jens Wille                           #
 #                                                                             #
 # Lingo is free software; you can redistribute it and/or modify it under the  #
 # terms of the GNU Affero General Public License as published by the Free     #
@@ -126,15 +126,6 @@ class Lingo
 
       def send_msg(msg)
         @filter ? forward(msg) : warn(msg)
-      end
-
-    end
-
-    class DebugFilter < Debugger
-
-      def init
-        @filter = true
-        super('')
       end
 
     end
